844259b6c90f972a3fec9ec42b3fdc92780f7f56
murilo-simoes <murilorsimoes@gmail.com>
2024-10-07 13:28:28 -0300

    Java add

---
+++ LoopinoApi/src/main/java/com/loopino/loopino/LoopinoApplication.java
@@ -0,0 +1,13 @@
+package com.loopino.loopino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoopinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoopinoApplication.class, args);
+	}
+
+}
---
+++ LoopinoApi/src/test/java/com/loopino/loopino/LoopinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loopino.loopino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoopinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
